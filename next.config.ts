3783be59f93436765c46c5602caa53c16924ca35
takkuAny <takashi.motoshima19820218@gmail.com>
2025-07-27 23:50:38 +0900

    commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['lh3.googleusercontent.com'],
     remotePatterns: [
