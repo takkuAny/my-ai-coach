cb43241c6e4c5badf394faab09d695f60d616634
takkuAny <takashi.motoshima19820218@gmail.com>
2025-07-28 08:21:53 +0900

    bug fix

--- src/app/lib/supabase/client.ts
+++ src/app/lib/supabase/client.ts
@@ -1,7 +1,9 @@
-// lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js'
+'use client'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { Database } from '@/lib/database.types'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createPagesBrowserClient<Database>({
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+})
